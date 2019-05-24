d07fe879fe7b5fe833da32b0308514e43db04662
Salvador Marín i Montero <50679238+smmsvf@users.noreply.github.com>
2019-05-24 14:23:30 +0200

    Add files via upload

--- CayeneClient-22-Maig-2019-Condicionat-Salvador-persones.py
+++ CayeneClient-22-Maig-2019-Condicionat-Salvador-persones.py
@@ -1,11 +1,14 @@
 import cayenne.client
 from random import randint
-from random import *
 from time import sleep
 
 
+
 def cayene(persones):
-    client.virtualWrite(persones)
+    client.virtualWrite(1,persones)
+    
+
+    
 
 def distancia():
     persones1 = randint(0,1)
@@ -13,23 +16,23 @@ def distancia():
   
 
 # Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-MQTT_USERNAME = "cabb3650-7c77-11e9-beb3-736c9e4bf7d0"
-MQTT_PASSWORD = "0abe0b568fd0ba4a0648c59c7d5a3a71ec534e7d"
-MQTT_CLIENT_ID = "93e9c2d0-7df9-11e9-ace6-4345fcc6b81e"
+MQTT_USERNAME = "2dd7a530-d092-11e8-a056-c5cffe7f75f9"
+MQTT_PASSWORD = "6d63b6a30a53c35a4abd3bc1b1876c470738a485"
+MQTT_CLIENT_ID = "00035950-7c78-11e9-be3b-372b0d2759ae"
 client = cayenne.client.CayenneMQTTClient()
 client.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID)
+
 
 while True:
     client.loop()
     persones = 0
     for segons in range(60):
         persones2 = distancia()
-        if persones2 == 0:
+        if persones2 == 1:
             persones += 1
-            print(persones)
-##   cayenne(persones)
-    sleep(2)
-
+        sleep(1)
+    cayene(persones)
+    
 
 
 
